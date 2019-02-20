8d43c175b9a98738f1d3ca165e89fa2de836bc22
brnpapa <bruno.papa@hotmail.com>
2019-02-20 16:10:13 -0300

    aula 19-02-2k19

--- src/1091.cpp
+++ src/1091.cpp
@@ -6,15 +6,14 @@ int main() {
     int k=1, n, m, x, y;
     cin >> k;
     do {
-        //leste-oeste    sul-norte
+        //leste-oeste, sul-norte
         cin >> n >> m;
         for (int i = 0; i < k; i++) {
             cin >> x >> y;
             if (x == n || y == m)
                 printf("divisa\n");
-            //leste
             else {
-
+                //leste
                 if (x > n) {
                     if (y > m) {
                         printf("NE\n");
