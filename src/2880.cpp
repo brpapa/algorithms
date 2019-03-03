0f4d6d297252817b091176077a11ef933cf3326a
brnpapa <bruno.papa@hotmail.com>
2019-03-02 21:16:30 -0300

    self-training

---
+++ src/2880.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int contaPosPossiveis(string msg, string crib)
+{
+    int soma = 0;
+    //para cada possibilidade i, começando em msg[i]
+    for (int i = 0; i <= (msg.size()-crib.size()); i++)
+    {
+        bool ok = true;
+        //para cada letra de crib
+        for (int j = 0; j < crib.size() && ok; j++)
+            if (crib.at(j) == msg.at(j+i))
+                ok = false;
+        if (ok)
+            soma++;
+    }
+    return soma;
+}
+int main()
+{
+    string msg, crib;
+    cin >> msg >> crib;
+    cout << contaPosPossiveis(msg, crib) << endl;
+    return 0;
+}
