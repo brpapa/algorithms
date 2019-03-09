ffde9df3584d639420e4171fea7b3d2adf54bf43
brnpapa <bruno.papa@hotmail.com>
2019-03-09 02:26:54 -0300

    up

---
+++ judges/judges/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  judges
+//
+//  Created by Bruno Papa on 08/03/19.
+//  Copyright © 2019 Bruno Papa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
