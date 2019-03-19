c990269d2b921de4034bf79fcf97ab4fbad48f7d
brnpapa <bruno.papa@hotmail.com>
2019-03-19 10:39:13 -0300

    12-03

---
+++ src/uva-10954.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int n, aux;
+    while (true)
+    {
+        cin >> n;
+        if (n == 0)
+            return 0;
+        
+        priority_queue<int> heap;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> aux;
+            heap.push(-aux);
+        }
+
+        int custo = 0;
+        while (!heap.empty())
+        {
+            int tmp1 = -heap.top();
+            heap.pop();
+            if (heap.empty())
+                break;
+            int tmp2 = -heap.top();
+            heap.pop();
+            custo += tmp1 + tmp2;
+            heap.push(-tmp1-tmp2);
+        }    
+        cout << custo << endl;
+    }
+}
