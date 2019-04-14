719eb5fd4728ae4e90863790b5d3a0e30cb5c056
brnpapa <bruno.papa@hotmail.com>
2019-04-14 13:14:21 -0300

    maximum increase

---
+++ codeforces/702A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int n, atual, ant = 0, maxAtual = 0, maxTotal = 1;
+   scanf("%d", &n);
+   for (int i = 0; i < n; i++)
+   {
+      scanf("%d", &atual);
+      if (atual > ant)
+      {
+         maxAtual++;
+         if (maxAtual > maxTotal)
+            maxTotal = maxAtual;
+      }
+      else maxAtual = 1;
+
+      ant = atual;
+   }
+   printf("%d\n", maxTotal);
+   return 0;
+}
