94f9c179ce367354892bc172fee16d8b3ef86044
brnpapa <bruno.papa@hotmail.com>
2019-04-14 21:37:31 -0300

    ilya and queries

---
+++ codeforces/313B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+   int m, l, r, soma[100001];
+   string str;
+   cin >> str >> m;
+
+   soma[1] = 0;
+   for (int i = 1; i < str.length(); i++)
+   {
+      soma[i+1] = soma[i];
+      if (str[i] == str[i-1])
+         soma[i+1]++;
+   }
+
+   while (m--)
+   {
+      scanf("%d %d", &l, &r);
+      printf("%d\n", soma[r]-soma[l]);
+   }
+   return 0;
+}
