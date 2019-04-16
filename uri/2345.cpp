e9382214cfd414a11f3fe177fdebc810d99b974b
brnpapa <bruno.papa@hotmail.com>
2019-04-16 16:04:12 -0300

    assigning teams

---
+++ uri/2345.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a, b, c, d, min, aux;
+   scanf("%d %d %d %d", &a, &b, &c, &d);
+   min = abs((a+b) - (c+d));
+
+   aux = abs((a+c) - (b+d));
+   if (aux < min) min = aux;
+   
+   aux = abs((a+d) - (b+c));
+   if (aux < min) min = aux;
+
+   printf("%d\n", min);
+   return 0;
+}
