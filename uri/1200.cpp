a22ac481dbc904a05a7f818ac5cfde32c0d4e678
brnpapa <bruno.papa@hotmail.com>
2019-04-16 16:03:29 -0300

    abb

---
+++ uri/1200.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+typedef struct llde
+{
+   char info;
+   struct llde *esq, *dir;
+} * node;
+
+int qteTotal, qte;
+
+void insere(node &arv, char info)
+{
+   if (arv == NULL)
+   {
+      node p = (node)malloc(sizeof(struct llde));
+      p->info = info;
+      p->esq = p->dir = NULL;
+      arv = p;
+      qteTotal++;
+   }
+   else
+   {
+      if (info < arv->info)
+         insere(arv->esq, info);
+      if (info > arv->info)
+         insere(arv->dir, info);
+   }
+}
+void prefix(node arv)
+{
+   if (arv == NULL)
+      return;
+   printf("%c%c", arv->info, ((qte++) == qteTotal-1)?'\n': ' ');
+   prefix(arv->esq);
+   prefix(arv->dir);
+}
+void infix(node arv)
+{
+   if (arv == NULL)
+      return;
+   infix(arv->esq);
+   printf("%c%c", arv->info, ((qte++) == qteTotal-1)?'\n': ' ');
+   infix(arv->dir);
+}
+void posfix(node arv)
+{
+   if (arv == NULL)
+      return;
+   posfix(arv->esq);
+   posfix(arv->dir);
+   printf("%c%c", arv->info, ((qte++) == qteTotal-1)?'\n': ' ');
+}
+bool busca(node arv, char info)
+{
+   if (arv == NULL)
+      return false;
+   if (info == arv->info)
+      return true;
+   if (info < arv->info)
+      return busca(arv->esq, info);
+   if (info > arv->info)
+      return busca(arv->dir, info);
+}
+int main()
+{
+   string str;
+   node arv = NULL;
+   qteTotal = 0;
+   while (getline(cin, str))
+   {
+      if (str.size() == 3)
+      {
+         if (str[0] == 'I')
+            insere(arv, str[2]);
+         if (str[0] == 'P')
+            printf("%c%sexiste\n", str[2], busca(arv, str[2])? " ": " nao ");
+      }
+      else
+      {
+         if (str == "INFIXA")
+         {
+            qte = 0;
+            infix(arv);
+         }
+         if (str == "PREFIXA")
+         {
+            qte = 0;
+            prefix(arv);
+         }
+         if (str == "POSFIXA")
+         {
+            qte = 0;
+            posfix(arv);
+         }
+      }
+   }
+   return 0;
+}
