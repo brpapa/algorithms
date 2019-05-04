5e7c0bf39f63d5ad9e03c7a5b686c0c9ea9708bf
brnpapa <bruno.papa@hotmail.com>
2019-05-04 18:53:16 -0300

    ad-hoc

---
+++ uri/1217.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+   int n, d, aluno;
+   while (true)
+   {
+      cin >> n >> d;
+      if (n == 0 && d == 0)
+         return 0;
+
+      vector<bool> participou(true, n);
+
+      for (int j = 0; j < d; j++)
+         for (int i = 0; i < n; i++)
+         {
+            cin >> aluno;
+            if (aluno == 0)
+               participou[i] = false;
+         }
+
+      bool achou = false;
+      for (int i = 0; i < n && !achou; i++)
+         if (participou[i] == true)
+            achou = true;
+      if (!achou)
+         cout << "no" << endl;
+      else
+         cout << "yes" << endl;
+   }
+   return 0;
+}
---
+++ uri/1318.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+   int qteBilhetes, qtePessoas, aux;
+   while (true)
+   {
+      cin >> qteBilhetes >> qtePessoas;
+      if (qteBilhetes == 0 && qtePessoas == 0)
+         return 0;
+
+      vector<int> bilhetes(qteBilhetes + 1, 0);
+      for (int i = 0; i < qtePessoas; i++)
+      {
+         cin >> aux;
+         bilhetes[aux]++;
+      }
+
+      int soma = 0;
+      for (int i = 1; i <= qteBilhetes; i++)
+         if (bilhetes[i] > 1)
+            soma++;
+
+      cout << soma << endl;
+   }
+   return 0;
+}
